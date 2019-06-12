62697e41b457a228e78d2365c75e2a1d0a2cb052
Adrian Popan <adrian.popan@gmail.com>
2019-06-12 17:38:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,11 @@ def assign_rooms(attendees)
   end
 end
 
-def printer
-  puts "XXX"
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assigned_room|
+    puts assigned_room
+  end
 end
