1e6d0f8d8572b20073615d50dac43ed410fbb847
Adrian Popan <adrian.popan@gmail.com>
2019-06-12 17:28:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(attendees)
   attendees.map do |name|
-    "Hello, my name is #{name}"
+    "Hello, my name is #{name}."
   end
 end
